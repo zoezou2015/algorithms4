2ae1bfeef9b3f14aaa633dff11d9ec272346785e
yanyan <zoe.yyzou@gmail.com>
2018-01-31 20:29:33 +0800

    Add one class for Fixed Capacity Stack data structure

---
+++ src/chapter1/FixedCapacityStack.java
@@ -0,0 +1,37 @@
+package chapter1;
+
+public class FixedCapacityStack {
+
+	private String[] stack;
+	// The pointer of the stack
+	private int N;
+	// The capacity of the stack, i.e., the length of the array
+	private int cap;
+
+	public FixedCapacityStack(int cap) {
+		this.cap = cap;
+		stack = new String[cap];
+	}
+
+	public void push(String s) {
+		if (this.N <= this.cap)
+			stack[this.N++] = s;
+		else
+			throw new RuntimeException("The stack is full! Pointer is: " + this.N + " of capacity " + this.cap);
+	}
+
+	public String pop() {
+		if (this.N != 0)
+			return stack[--this.N];
+		else
+			throw new RuntimeException("The stack is null!");
+	}
+
+	public boolean isEmpty() {
+		return N == 0;
+	}
+
+	public int size() {
+		return this.cap;
+	}
+}
